1ecd4e14897128a28c248eae583ba494f46d50ab
Sayemum Hassan <sayemumhassan@gmail.com>
2024-08-28 10:36:49 -0400

    Code Progress

--- generator.py
+++ generator.py
@@ -1,10 +1,30 @@
-import random
+from typing import List
 
-def lottery():
-    for i in range(6):
-        yield random.randint(1,40)
+def all_distances(numbers: List[float]) -> List[List[float]]:
+    """Return a nxn two-dimensional list containing the pairwise distances
+    between all n provided numbers, where entry (i, j) contains the absolute 
+    value of the difference between the i'th and j'th entry in the list.
+
+    >>> all_distances([0.0, 1.0, 3.0])
+    [[0.0, 1.0, 3.0], 
+     [1.0, 0.0, 2.0], 
+     [3.0, 2.0, 0.0]]
     
-    yield random.randint(1,15)
+    """
+    # YOUR CODE HERE
+    #raise NotImplementedError()
+    n = len(numbers)
 
-for random_num in lottery():
-    print("And the next number is... %d!" % (random_num))
+    # matrix = [[x] for x in range(n)]
+    matrix = [0] * n
+    #for i in range(n):
+        #new_list = [n]
+        #matrix.append()
+    
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = abs(matrix[i] - matrix[j])
+
+    return matrix
+
+print(all_distances([0.0, 1.0, 3.0]))
